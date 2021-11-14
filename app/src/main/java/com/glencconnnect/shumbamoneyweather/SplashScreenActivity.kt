9456a5fde8de75b6e57e9f83a867123d4a4955de
glenchiridza <glenchiridz@gmail.com>
2021-11-14 16:43:27 +0200

    splash screen

--- app/src/main/java/com/glencconnnect/shumbamoneyweather/SplashScreenActivity.kt
+++ app/src/main/java/com/glencconnnect/shumbamoneyweather/SplashScreenActivity.kt
@@ -1,11 +1,20 @@
 package com.glencconnnect.shumbamoneyweather
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 
 class SplashScreenActivity : AppCompatActivity() {
+    private val handler = Handler()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        handler.postDelayed({
+            startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+            finish()
+        }, 3000)
     }
 }
