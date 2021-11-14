f0335e014a0fc2ecbb96392b814b925b934f59c9
glenchiridza <glenchiridz@gmail.com>
2021-11-14 16:47:16 +0200

    detail view

---
+++ app/src/main/java/com/glencconnnect/shumbamoneyweather/ui/DetailView.kt
@@ -0,0 +1,10 @@
+package com.glencconnnect.shumbamoneyweather.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
