b86adea5af676498bb13e64b8768d16c5db27542
glenchiridza <glenchiridz@gmail.com>
2021-11-14 18:49:08 +0200

    about activity

--- app/src/main/java/com/glencconnnect/shumbamoneyweather/MainActivity.kt
+++ app/src/main/java/com/glencconnnect/shumbamoneyweather/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.glencconnnect.shumbamoneyweather
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.glencconnnect.shumbamoneyweather.adapters.WeatherRecyclerAdapter
+import com.glencconnnect.shumbamoneyweather.ui.AboutActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -22,5 +24,16 @@ class MainActivity : AppCompatActivity() {
         recycler_view.layoutManager = layoutManager
         recycler_view.adapter = adapter
         recycler_view.setHasFixedSize(true)
+    }
+
+    private fun shareIntent() {
+        val shareIntent = Intent()
+        shareIntent.action = Intent.ACTION_SEND
+        startActivity(Intent.createChooser(shareIntent, "Share App"))
+    }
+
+    private fun retrieveCredits() {
+        val intent = Intent(this, AboutActivity::class.java)
+        startActivity(intent)
     }
 }
