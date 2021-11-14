e7df7aa3c49f3259319dde2ff68649652c679614
glenchiridza <glenchiridz@gmail.com>
2021-11-14 21:05:11 +0200

    theme fix

--- app/src/main/java/com/glencconnnect/shumbamoneyweather/ui/AboutActivity.kt
+++ app/src/main/java/com/glencconnnect/shumbamoneyweather/ui/AboutActivity.kt
@@ -2,6 +2,7 @@ package com.glencconnnect.shumbamoneyweather.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.glencconnnect.shumbamoneyweather.MainActivity
 import com.glencconnnect.shumbamoneyweather.R
 import kotlinx.android.synthetic.main.activity_about.*
 
@@ -9,20 +10,24 @@ class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+
+        //get intent extra and customize activity output
+        val action = intent.getStringExtra(MainActivity.CHOICE_EXTRA)
+        action?.let { switchThroughViewText(it) }
     }
 
     private fun switchThroughViewText(action: String) {
         when (action) {
+            //custom staff for the about section
             "action_about" -> {
                 abt_title.text = getString(R.string.shumba_money_weather)
                 abt_description.text = getString(R.string.about_text)
             }
+            //custom staff for the credits section
             "action_credits" -> {
                 abt_title.text = getString(R.string.andro_version)
                 abt_description.text = getString(R.string.developer_info)
-            }
-            else -> {
-                //do nothing, show the template view default set information
             }
         }
     }
