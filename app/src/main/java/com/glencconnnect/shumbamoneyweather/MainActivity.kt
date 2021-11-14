ae9837207a511e437e33538dce33c9bd256640de
glenchiridza <glenchiridz@gmail.com>
2021-11-14 20:09:15 +0200

    intent share

--- app/src/main/java/com/glencconnnect/shumbamoneyweather/MainActivity.kt
+++ app/src/main/java/com/glencconnnect/shumbamoneyweather/MainActivity.kt
@@ -2,6 +2,8 @@ package com.glencconnnect.shumbamoneyweather
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.glencconnnect.shumbamoneyweather.adapters.WeatherRecyclerAdapter
@@ -11,7 +13,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
 
-    private lateinit var adapter:WeatherRecyclerAdapter;
+    private lateinit var adapter:WeatherRecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +26,25 @@ class MainActivity : AppCompatActivity() {
         recycler_view.layoutManager = layoutManager
         recycler_view.adapter = adapter
         recycler_view.setHasFixedSize(true)
+
+        fab.setOnClickListener {
+            shareIntent()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.options, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if (id == R.id.action_about) {
+            retrieveCredits()
+        } else if (id == R.id.action_credits) {
+            retrieveCredits()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun shareIntent() {
