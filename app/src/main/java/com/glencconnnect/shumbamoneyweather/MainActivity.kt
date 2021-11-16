6039d66b05f3c3184d58929d9c3dc9f2946c6297
glenchiridza <glenchiridz@gmail.com>
2021-11-17 00:46:38 +0200

    detail view done

--- app/src/main/java/com/glencconnnect/shumbamoneyweather/MainActivity.kt
+++ app/src/main/java/com/glencconnnect/shumbamoneyweather/MainActivity.kt
@@ -2,72 +2,87 @@ package com.glencconnnect.shumbamoneyweather
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.glencconnnect.shumbamoneyweather.R
 import com.glencconnnect.shumbamoneyweather.adapters.WeatherRecyclerAdapter
 import com.glencconnnect.shumbamoneyweather.models.OuterContainer
 import com.glencconnnect.shumbamoneyweather.retrofit.RetrieveWeather
 import com.glencconnnect.shumbamoneyweather.retrofit.ServiceBuilder
 import com.glencconnnect.shumbamoneyweather.ui.AboutActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_splash_screen.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    private var adapter: WeatherRecyclerAdapter? = null
 
-    private lateinit var adapter:WeatherRecyclerAdapter
-    companion object {const val CHOICE_EXTRA = "CHOICE_EXTRA"}
-
-    private var weatherList: List<OuterContainer>? = null
+    private var weatherList: ArrayList<com.glencconnnect.shumbamoneyweather.models.List>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-        weatherList = ArrayList()
-        //setup recycler view
-        adapter = weatherList?.let { WeatherRecyclerAdapter(this, it) }!!
 
-        val layoutManager = LinearLayoutManager(this)
-        recycler_view.layoutManager = layoutManager
+        weatherList = ArrayList<com.glencconnnect.shumbamoneyweather.models.List>()
+        adapter = WeatherRecyclerAdapter(this, weatherList!!)
+        val manager = LinearLayoutManager(this)
+        recycler_view.layoutManager = manager
         recycler_view.adapter = adapter
         recycler_view.setHasFixedSize(true)
-
-        fab.setOnClickListener {
-            shareIntent()
-        }
+        progressing.visibility = View.VISIBLE
+        fab.setOnClickListener(View.OnClickListener { view: View? -> shareIntent() })
 
 
-        var retrieveWeather: RetrieveWeather = ServiceBuilder.builderService(RetrieveWeather::class.java)
+        //called on create
+        retrieveWeatherData()
 
-
-
-        retrieveWeather.data?.enqueue(
-                object : Callback<OuterContainer?> {
-                    override fun onResponse(call: Call<OuterContainer?>, response: Response<OuterContainer?>) {
-
-                        //textView.setText(response.body().toString());
-                        if (response.isSuccessful) {
-                            weatherList = response.body() as List<OuterContainer>?
-                            adapter.setDataListing(weatherList)
-                        }
-                    }
-
-                    override fun onFailure(call: Call<OuterContainer?>, t: Throwable) {
-
-
-                        Toast.makeText(this@MainActivity, t.message + "-----" + t.cause, Toast.LENGTH_SHORT).show()
-                    }
-                })
-
+        //click fab to refresh, to try retrieve again if failed on oncreate first view
+        fab_refresh.setOnClickListener(View.OnClickListener {
+            progressing.visibility = View.VISIBLE
+            textView.visibility = View.GONE
+            retrieveWeatherData()
+        })
     }
 
+    private fun retrieveWeatherData() {
+        val retrieveWeather: RetrieveWeather = ServiceBuilder.builderService(RetrieveWeather::class.java)
+
+        retrieveWeather.data?.enqueue(object : Callback<OuterContainer?> {
+            override fun onResponse(call: Call<OuterContainer?>, response: Response<OuterContainer?>) {
+                progressing!!.visibility = View.GONE
+                fab_refresh!!.visibility = View.GONE
+                textView!!.visibility = View.GONE
+                shumba_img!!.visibility = View.GONE
+                if (response.isSuccessful) {
+                    val listing: OuterContainer? = response.body()
+                    val list: ArrayList<com.glencconnnect.shumbamoneyweather.models.List> = listing!!.getList()
+                    weatherList = list
+                    adapter?.setDataListing(weatherList!!)
+                }
+            }
+
+            override fun onFailure(call: Call<OuterContainer?>, t: Throwable) {
+                progressing!!.visibility = View.GONE
+                shumba_img!!.visibility = View.VISIBLE
+                fab_refresh!!.visibility = View.VISIBLE
+                //the default error value in textview will be retained here from layout string resource, if no response
+                textView!!.visibility = View.VISIBLE
+            }
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.options, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val id = item.itemId
@@ -87,12 +102,15 @@ class MainActivity : AppCompatActivity() {
         startActivity(Intent.createChooser(shareIntent, "Share With"))
     }
 
-
     //retrieve information on about and credits, pass in the extras depending on the chosen option and dynamicaly populate that single activity with the right data
     //simply reusing the same AboutActiivty for two different options
     private fun retrieveInfo(choice_extra: String) {
         val intent = Intent(this, AboutActivity::class.java)
         intent.putExtra(CHOICE_EXTRA, choice_extra)
         startActivity(intent)
+    }
+
+    companion object {
+        var CHOICE_EXTRA = "CHOICE_EXTRA"
     }
 }
--- app/src/main/java/com/glencconnnect/shumbamoneyweather/ui/DetailView.kt
+++ app/src/main/java/com/glencconnnect/shumbamoneyweather/ui/DetailView.kt
@@ -1,12 +1,59 @@
 package com.glencconnnect.shumbamoneyweather.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 import com.glencconnnect.shumbamoneyweather.R
+import com.glencconnnect.shumbamoneyweather.adapters.WeatherRecyclerAdapter
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.detail_item.*
 
 class DetailView : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_view)
+
+        //set the values retrieved from intent, in accordance to their position in passed on ArrayList
+        val weatherData = intent.getStringArrayListExtra(WeatherRecyclerAdapter.WEATHER_DATA)
+        if (weatherData != null) {
+            //use substring to remove the time, and output date sequence only
+            dv_date!!.text = weatherData[0].substring(0, 11)
+
+            //use substring to remove the decimal on detail output only, for better ui rendering
+            val deg = StringBuilder().append(weatherData[1].substring(0, 2)).append('\u00B0')
+            dv_degrees!!.text = deg
+            val detail = StringBuilder()
+                    .append(getString(R.string.feels_like))
+                    .append(deg).append(" ").append(weatherData[2])
+            dv_weather_detail!!.text = detail
+            dv_humidity!!.text = weatherData[3]
+            dv_pressure!!.text = weatherData[4]
+            Picasso.get().load(weatherData[5]).into(dv_icon)
+
+
+            //calculate whether to output hot or cold based on degree condition given
+            //if temp > 25deg hot and if temp <10 deg cold
+            val degrees = weatherData[1].toDouble()
+            when {
+                degrees > 25 -> {
+                    dv_description.setText(R.string.hot)
+                }
+                degrees < 10 -> {
+                    dv_description.setText(R.string.cold)
+                }
+                else -> {
+                    dv_description!!.text = ""
+                }
+            }
+
+            //daytime,start from the index that outputs time only
+            dv_day_time!!.text = weatherData[0].substring(12)
+        }
     }
+
+
 }
